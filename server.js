f9fff5cda53e116c0635e94d45dbc03386a16aa8
Courtney Paasch <paasch.courtney@gmail.com>
2020-08-26 08:33:22 -0700

    add post function for new notes

--- server.js
+++ server.js
@@ -19,6 +19,33 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+//returns json of all notes
+app.get("/api/notes", (reg, res) => {
+    return res.json(api);
+});
+
+
+//posts the new note to the db.json file without overriding 
+app.post("/api/notes", (req, res) => {
+    let note = req.body;
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        const savedNotes = JSON.parse(data);
+        savedNotes.push(note)
+
+        fs.writeFile("./db/db.json", JSON.stringify(savedNotes), (err) => {
+            if (err) throw err;
+           console.log("Notes saved!");
+            }
+        );
+        
+        res.json(savedNotes)
+    });
+
+});
+
+
+
 //listener
 app.listen(PORT,() => {
     console.log(`App listening on PORT ${PORT}`);
