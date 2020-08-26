e0e27b34dfc931ab2ce5aec1ba1c73c1c9489885
Courtney Paasch <paasch.courtney@gmail.com>
2020-08-26 07:47:12 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+//establish initial requires and port 
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const api = require(".db/db.json");
+const app = express();
+const port = 3000;
+
