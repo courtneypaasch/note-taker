f43575bef41ffa0b73f414847b94ec1563dd69c6
Courtney Paasch <paasch.courtney@gmail.com>
2020-08-26 08:05:12 -0700

    add port listener and dependencies

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-//establish initial requires and port 
+//establish dependencies and express
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const api = require(".db/db.json");
+const api = require("./db/db.json");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.sendFile("./public/index.html");
+});
+
+app.get("/notes")
+
+//listener
+app.listen(PORT,() => {
+    console.log(`App listening on PORT ${PORT}`);
+})
