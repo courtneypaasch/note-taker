b9302dfe85744e42c12c589a50492bf034966098
Courtney Paasch <paasch.courtney@gmail.com>
2020-08-27 15:30:08 -0700

    update post function and add delete function

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const api = require("./db/db.json");
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.use(express.static(__dirname + "/public"));
 
 //directs homepage of port to index.html
 app.get("/", (req, res) => {
@@ -24,24 +27,33 @@ app.get("/api/notes", (reg, res) => {
     return res.json(api);
 });
 
+const writeAPI = (arr) => {
+    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(arr), (err) => {
+        if (err) throw err;
+        console.log("Notes saved!");
+        });
+}
 
 //posts the new note to the db.json file without overriding 
+let savedNotes;
 app.post("/api/notes", (req, res) => {
-    let note = req.body;
-    fs.readFile("./db/db.json", (err, data) => {
-        if (err) throw err;
-        const savedNotes = JSON.parse(data);
-        savedNotes.push(note)
+    let note = {
+        id: Math.floor(Math.random() * 100000000),
+        title: req.body.title,
+        text: req.body.text
+    }
+    api.push(note);
+    writeAPI(api);
+    res.send(savedNotes);
+});
 
-        fs.writeFile("./db/db.json", JSON.stringify(savedNotes), (err) => {
-            if (err) throw err;
-           console.log("Notes saved!");
-            }
-        );
-        
-        res.json(savedNotes)
-    });
-
+//deletes notes from the db.json file
+app.delete("/api/notes/:id", (req, res) => {
+    const deleteNote = parseInt(req.params.id);
+    const callAPI = JSON.parse(fs.readFileSync("./db/db.json").toString());
+    const editAPI = callAPI.filter((note) => note.id != deleteNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(editAPI))
+    res.send("It's outa here!")
 });
 
 
